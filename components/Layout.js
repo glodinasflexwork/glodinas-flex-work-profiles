dabbcd718948fbfbca1f079ba7c07f4e6f406332
Cihat Kaya <cihatkaya@Cihats-MacBook-Pro-2.local>
2025-04-28 23:29:47 +0200

    Include global favicon inside Layout component

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,15 @@
+import Head from 'next/head';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
 export default function Layout({ children }) {
   return (
     <div className="min-h-screen bg-white text-gray-800">
+      {/* Global Favicon */}
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <Navbar />
       <main>{children}</main>
       <Footer />
