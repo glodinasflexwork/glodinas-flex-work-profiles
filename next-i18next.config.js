bb57ebe2f3b95518377beac3cec57623bcacc75a
Cihat Kaya <cihatkaya@Cihats-MacBook-Pro-2.local>
2025-05-03 15:06:21 +0200

    Restore working files after cleaning Git history

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,9 @@
+const path = require('path');
+
 module.exports = {
   i18n: {
     defaultLocale: 'nl',
     locales: ['nl', 'en', 'de', 'pl', 'ro', 'bg'],
   },
+  localePath: path.resolve('./public/locales'),
 };
