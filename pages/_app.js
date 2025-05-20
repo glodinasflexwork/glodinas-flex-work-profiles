f2aee855e77731ec80bcb0b75aa0cc3dd71d033a
Glodinas Flex Work <angles.readier.7d@icloud.com>
2025-05-20 16:33:59 -0400

    Fix: Apply Layout component by default to restore navigation and footer

--- pages/_app.js
+++ pages/_app.js
@@ -2,16 +2,20 @@ import '../styles/globals.css';
 import { SessionProvider } from 'next-auth/react';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import { NotificationProvider } from '../components/NotificationContext';
+import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   const router = useRouter();
   
-  // Use getLayout if it's defined on the page, otherwise use default layout
-  const getLayout = Component.getLayout || ((page) => page);
-
+  // Use getLayout if it's defined on the page, otherwise use default Layout
+  const getLayout = Component.getLayout || ((page) => <Layout pageTitle={Component.pageTitle}>{page}</Layout>);
+  
   return (
     <SessionProvider session={session}>
-      {getLayout(<Component {...pageProps} />)}
+      <NotificationProvider>
+        {getLayout(<Component {...pageProps} />)}
+      </NotificationProvider>
     </SessionProvider>
   );
 }
